909b226c4afb6535363de390103367d4909dad21
Oscar-FSD <81850353+oscarchuaweiwen-fsd@users.noreply.github.com>
2022-03-05 19:57:45 +0800

    updated

--- background.js
+++ background.js
@@ -16,11 +16,19 @@ async function getTab() {
   return tabs[0];
 }
 
-chrome.tabs.onUpdated.addListener(async function () {
-  let tab = await getTab();
+chrome.storage.sync.get(['username','password','role'],result=>{
+  if(result.username =="" && result.password == "" && result.role == ""){
+    alert("Please kindly fill out your username and password in the extension settings. Thank you!")
+  }else{
+    chrome.tabs.onUpdated.addListener(async function () {
+      let tab = await getTab();
+    
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id, allFrames: true },
+        files: ["foregroup.js"],
+      });
+    });
+  }
+})
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id, allFrames: true },
-    files: ["foregroup.js"],
-  });
-});
+
